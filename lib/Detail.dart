4a1f736450a8473df9dd74f89f27812b8fa5fcbf
William Vitorino <williamvitorino3@gmail.com>
2019-08-22 01:47:15 -0300

    Calculo do tamanho da tela.

--- lib/Detail.dart
+++ lib/Detail.dart
@@ -14,6 +14,12 @@ class Detail extends StatefulWidget {
 
 class _DetailState extends State<Detail> {
   
+  int maxWidth (BuildContext context) {
+    int width = int.parse((MediaQuery.of(context).size.width*0.037).toStringAsFixed(0));
+    print("WIDTH: ${width}");
+    return width;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -22,56 +28,67 @@ class _DetailState extends State<Detail> {
         backgroundColor: Colors.cyan[700],
         centerTitle: true,
       ),
-      body: new Card(
-        margin: EdgeInsets.all(0.0),
-        elevation: 10.0,
-        color: Colors.cyan[700],
-        child: new Container(
-          padding: EdgeInsets.all(10.0),
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              new Image.network(
-                "${widget.snapshot.data["image"]}",
-                alignment: Alignment.topLeft,
-                fit: BoxFit.cover,
-                width: MediaQuery.of(context).size.width*0.95,
-                // scale: 3.5,
-              ),
-              new Container(
-                margin: EdgeInsets.only(top: 10.0),
-                width: MediaQuery.of(context).size.width*0.75,
-                height: MediaQuery.of(context).size.height*0.45,
-                child: new Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    new Text(
-                      widget.snapshot.data["title"],
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.bold,
-                      ),
+      body: new ListView.builder(
+        itemCount: 1,
+        itemBuilder: ((context, index) {
+          return new Container(
+            child: new Column(
+              children: <Widget>[
+                new Card(
+                  margin: EdgeInsets.all(0.0),
+                  elevation: 10.0,
+                  color: Colors.cyan[700],
+                  child: new Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        new Image.network(
+                          "${widget.snapshot.data["imagem"]}",
+                          alignment: Alignment.topLeft,
+                          fit: BoxFit.cover,
+                          width: MediaQuery.of(context).size.width*0.95,
+                          // scale: 3.5,
+                        ),
+                        new Container(
+                          margin: EdgeInsets.only(top: 10.0),
+                          width: MediaQuery.of(context).size.width*0.75,
+                          height: MediaQuery.of(context).size.height*0.45,
+                          child: new Column(
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              new Text(
+                                widget.snapshot.data["titulo"],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 25.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              new SizedBox(height: 5.0,),
+                              new Text(
+                                widget.snapshot.data["descricao"],
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 17.5
+                                ),
+                                maxLines: this.maxWidth(context),
+                                overflow: TextOverflow.fade,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                    new SizedBox(height: 5.0,),
-                    new Text(
-                      widget.snapshot.data["content"],
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 17.5
-                      ),
-                      maxLines: 17,
-                      overflow: TextOverflow.fade,
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
-      ),
+              ],
+            ),
+          );
+        })
+      )
     );
   }
 }
